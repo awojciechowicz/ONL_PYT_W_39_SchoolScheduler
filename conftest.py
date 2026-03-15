3c7d0f037f7c45a67d691b401dbfb3c3dcb54488
awojciechowicz <adrianwojciechowicz@gmail.com>
2026-03-15 14:02:39 +0100

    Timeslot fixture

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
+import datetime
 import pytest
 
-from schedule_app.models import Subject, Teacher
+from schedule_app.models import Subject, Teacher, TimeSlot
 
 
 @pytest.fixture
@@ -13,4 +14,11 @@ def teacher():
         first_name="Test teacher name",
         last_name="Test teacher surname",
         email="Test teacher email"
+    )
+
+@pytest.fixture
+def timeslot():
+    return TimeSlot.objects.create(
+        start_time=datetime.time(hour=8, minute=00),
+        end_time=datetime.time(hour=8, minute=45),
     )
